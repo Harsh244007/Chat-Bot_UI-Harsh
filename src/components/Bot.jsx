83f902b46f9523a6a1a666f1e60408ee44e92b43
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-06-24 23:20:41 +0530

    main

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -4,7 +4,7 @@ import "./Bot.css"
 export default function Bot() {
    
     return (
-        <div style={{
+        <div  style={{
             float: "right", margin: "0.5vw 0.5vw 0vw 2vw", display: "block", height: "27vw", width: "35vw", color:"black"}}>
             <div style={{
                 backgroundColor: 'darkorange', height: "100%",
@@ -12,7 +12,7 @@ export default function Bot() {
                 borderRadius: "3vw 3vw 0vw 0vw"
             }}>
                 
-                <div style={{padding:"1vw 2vw 6vw 8vw",justifyContent:"center",alignItems:"center",}}>
+                <div className='mainiris' style={{padding:"1vw 2vw 6vw 8vw",justifyContent:"center",alignItems:"center",}}>
                     <p style={{fontSize:"2.0vw"}}>IRIS</p>
                     <p style={{fontSize:"1.4vw"}}>Hello 👋</p>
                     <p style={{fontSize:"1.4vw"}}>I am Iris, a Virtual Assitant</p>
@@ -22,10 +22,10 @@ export default function Bot() {
                     <div><img className='imageh' src={"https://www.svgrepo.com/show/91857/robot.svg"} alt="" /></div>
                     <div className='ha'>
                         <p className='hap'>Frequently Asked Questions ?</p>
-                        <button className="buttn"><p>Can i redeem my FB before the original term?</p></button>
-                        <button className="buttn"><p>How do I pat my Credit card bill?</p></button>
-                        <button className="buttn"><p> How can I get my Account Statement? </p></button>
-                        <button className="buttn"><p>What is the tenure of Fixed Deposit?</p></button>
+                        <button className="buttn1"><p>Can I redeem my FB before the original term?</p></button>
+                        <button className="buttn2"><p>How do I pay my Credit card bill?</p></button>
+                        <button className="buttn3"><p> How can I get my Account Statement? </p></button>
+                        <button className="buttn4"><p>What is the tenure of Fixed Deposit?</p></button>
                         <button className='final'><p>Start a New Conversation</p></button>
                 </div>
                 
